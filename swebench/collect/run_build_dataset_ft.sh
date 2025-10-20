3eeae779267d05a0c84d377b5ce152d080f36394
d0rbu <hacperu2010@gmail.com>
2025-10-20 18:49:51 -0500

    make collection scripts specific

--- swebench/collect/run_build_dataset_ft.sh
+++ swebench/collect/run_build_dataset_ft.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 uv run python build_dataset_ft.py \
-    --instances_path "<path to folder containing task instance (raw) files>" \
-    --output_path "<path to folder to save finetuning dataset to>" \
-    --eval_path "<path to folder containing all evaluation task instances>"
+    --instances_path "tasks" \
+    --output_path "ft_tasks" \
+    --eval_path "eval_tasks"
--- swebench/collect/run_get_tasks_pipeline.sh
+++ swebench/collect/run_get_tasks_pipeline.sh
@@ -6,5 +6,5 @@
 
 uv run python get_tasks_pipeline.py \
     --repos 'scikit-learn/scikit-learn', 'pallets/flask' \
-    --path_prs '<path to folder to save PRs to>' \
-    --path_tasks '<path to folder to save tasks to>'
+    --path_prs 'prs' \
+    --path_tasks 'tasks'
